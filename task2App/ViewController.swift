230bfb5c4406246133b776c26fe2c04d88908582
yasu1125 <v22.m1125@gmail.com>
2021-03-17 12:59:17 +0900

    Initial Commit

---
+++ task2App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task2App
+//
+//  Created by yasudamasato on 2021/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
