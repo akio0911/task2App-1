a8f55294ed216647d4438b5a2bc0d9391105ee78
yasu1125 <v22.m1125@gmail.com>
2021-03-20 05:18:36 +0900

    initial commit

--- task2App/ViewController.swift
+++ task2App/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var resultLabel: UILabel!
+
+    var result: Double = 0.0
+
+    @IBAction func selectOperator(_ sender: UISegmentedControl) {
+
+        let number1: Double = Double(textField1.text!) ?? 0
+        let number2: Double = Double(textField2.text!) ?? 0
+
+        if sender.selectedSegmentIndex == 0 {
+            result = number1 + number2
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            result = number1 - number2
+        }
+        else if sender.selectedSegmentIndex == 2 {
+            result = number1 * number2
+        }
+        else if sender.selectedSegmentIndex == 3 {
+            if number2 == 0 {
+                textField2.text = "1以上を入力してください"
+                return
+            }else {
+                result = number1 / number2
+            }
+        }
     }
 
+    @IBAction func pressButton(_ sender: Any) {
+        resultLabel.text = String(result)
 
+    }
 }
 
